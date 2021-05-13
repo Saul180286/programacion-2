0e49eac79a1ff4f1eed777fd443b3e8ee37acfa4
Óscar Miranda <oscarmiranda3615@gmail.com>
2021-05-13 13:57:03 -0600

    Agregar constructor para cargar datos de archivo

--- Dataset.h
+++ Dataset.h
@@ -1,6 +1,8 @@
 #ifndef DATASET_H
 #define DATASET_H
 
+#include <string>
+#include <sstream>
 #include <fstream>
 #include "Point.h"
 
@@ -9,14 +11,42 @@ class Dataset{
 		Point* points[150];
 		int index;
 		int size;
-
-		ifstream myfile;
-		string filename;
-		int features;
 	public:
 		Dataset(int size) {
 			this->index = 0;
 			this->size = size;
+		}
+
+		Dataset(string filename) {
+			ifstream csv(filename);
+			string row, tmpNumber;
+			int columnas, filas;
+
+			// Obtener primera línea: filas y columnas.
+			getline(csv, row);
+			stringstream fila1(row); // Permite usar getline en string
+			getline(fila1, tmpNumber, ' '); filas = stoi(tmpNumber);
+			getline(fila1, tmpNumber); columnas = stoi(tmpNumber);
+
+			// Inicializar valores como en el constructor por defecto.
+			this->index = 0;
+			this->size = filas;
+
+			// Rellenar el data set con los puntos.
+			while (getline(csv, row)) {
+				stringstream line(row);
+				float* x = new float[columnas];
+				string label;
+
+				for (int i = 0; i < columnas; i++) {
+					getline(line, tmpNumber, ' '); // Obtiene cada x hasta encontrar un espacio.
+					x[i] = stof(tmpNumber); // stof: string -> float
+				}
+				getline(line, label); // Obtiene el valor final de la fila.
+
+				Point* p = new Point(x, columnas, stoi(label));
+				this->add(p);
+			}
 		}
 
 		~Dataset() {
@@ -29,7 +59,7 @@ class Dataset{
 		}
 
 		void print() {
-			for (int i = 0; i < size; i++) {
+			for (int i = 0; i < index; i++) {
 				cout << "Index: " << i << "\t";
 				points[i]->print();
 			}
--- main.cpp
+++ main.cpp
@@ -4,33 +4,16 @@
 #include "kNN.h"
 
 int main() {
-	int points = 3;
-	int k = 3;
-	int classes = 3;
+	int k = 3, classes = 3;
+	int claseFinal;
 
-	Point* p1 = new Point(new float[2]{0, 1},  2, 0);
-	Point* p2 = new Point(new float[2]{1, 0},  2, 0);
-	Point* p3 = new Point(new float[2]{1, 1},  2, 0);
-
-	p1->print();
-	p2->print();
-	p3->print();
-
-	Dataset* d = new Dataset(points);
-	d->add(p1);
-	d->add(p2);
-	d->add(p3);
-
+	Dataset* d = new Dataset("iris.data.csv");
 	kNN knn(d, k, classes);
 
-	cout << "Clase pronosticada: ";
-	cout << knn.getClass(new Point( new float[2]{7, 7}, 2, 1)) << endl;
+	claseFinal = knn.getClass(new Point(new float[4]{7, 7, 7, 7}, 4, 1));
+	cout << "Clase pronosticada: " << claseFinal << endl;
 
 	delete d;
-
-	delete p1;
-	delete p2;
-	delete p3;
 
 	return 0;
 }
